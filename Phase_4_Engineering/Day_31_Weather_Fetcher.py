b6ac3025d2bbff8a8e1160a618d96e43aef0b1a3
Maksym <46499085+midit@users.noreply.github.com>
2026-03-10 09:29:57 +0000

    Day_44 add telegram bot with FSM, Day_31 switch to async aiohttp

--- Phase_4_Engineering/Day_31_Weather_Fetcher.py
+++ Phase_4_Engineering/Day_31_Weather_Fetcher.py
@@ -1,11 +1,11 @@
 import os
-import requests
+import aiohttp
 from dotenv import load_dotenv
 
 load_dotenv("Phase_4_Engineering/other/Day_31/.env")
 key = os.getenv("OPENWEATHER_API_KEY")
 
-def get_weather(city_name):
+async def get_weather(city_name):
     params = {
         'q': city_name,
         'appid': key,
@@ -13,17 +13,29 @@ def get_weather(city_name):
         'lang': 'ua' 
     }   
     url = "https://api.openweathermap.org/data/2.5/weather"
-    response = requests.get(url, params=params)
 
-    data = response.json()
-    if response.status_code == 200:
-        temp = data['main']['temp']
-        humidity = data['main']['humidity']
-        wind = data['wind']['speed']
-        desc = data['weather'][0]['description']
-        print(f"🌍 {city_name}: {temp}°C, {desc}. Вологість: {humidity}%, Вітер: {wind} м/с")
-    else:
-        print(f"[!] Помилка для міста {city_name}: {data.get('message')}")
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=params) as response:
+            data = await response.json()
+            if response.status == 200:
+                temp = data['main']['temp']
+                desc = data['weather'][0]['description']
+                return f"🌍 {city_name}: {temp}°C, {desc}."
+            else:
+                return f"⚠️ Місто {city_name} не знайдено."
+    
+    # - - - - Old Implementation - - - -
+    # response = requests.get(url, params=params)
+
+    # data = response.json()
+    # if response.status_code == 200:
+    #     temp = data['main']['temp']
+    #     humidity = data['main']['humidity']
+    #     wind = data['wind']['speed']
+    #     desc = data['weather'][0]['description']
+    #     print(f"🌍 {city_name}: {temp}°C, {desc}. Вологість: {humidity}%, Вітер: {wind} м/с")
+    # else:
+    #     print(f"[!] Помилка для міста {city_name}: {data.get('message')}")
 
 if __name__ == "__main__":
     get_weather("Київ")
